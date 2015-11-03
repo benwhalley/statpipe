3a02bac256627f8f16c96dcffb09eb22cee3339f
Ben Whalley <benwhalley@gmail.com>
2015-11-03 15:10:17 +0000

    changed project url

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='Statpipe',
-    version='0.1.8',
+    version='0.1.9',
     author='Ben Whalley',
     author_email='benwhalley@gmail.com',
     packages=['statpipe'],
     scripts=['bin/statpipe', 'bin/statpipe_image'],
-    url='http://pypi.python.org/pypi/statpipe/',
+    url='https://github.com/benwhalley/statpipe',
     license='LICENSE.txt',
     description='Pipe stuff to Stata, get results back.',
     long_description=open('README.rst').read(),
